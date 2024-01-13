d3a02b55debc109b1b7659ce0fb63a9163a71b2e
teddy5025 <teddy5025@naver.com>
2024-01-13 15:36:27 +0900

    CardDao 1차 제조

--- src/main/java/com/sg/back/card/dao/CardDao.java
+++ src/main/java/com/sg/back/card/dao/CardDao.java
@@ -1,5 +1,24 @@
 package com.sg.back.card.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CardDao {
+    Logger logger = LoggerFactory.getLogger(CardDao.class);
+    @Autowired
+    SqlSessionTemplate sqlSessionTemplate=null;
+    public List<Map<String, Object>> getCard(Map<String, Object> cmap) {
+        logger.info("carddao : getCard");
+        List<Map<String,Object>> cardList=null;
+        cardList=sqlSessionTemplate.selectList("getCard",cmap);
+        return cardList;
+    }
     
 }
