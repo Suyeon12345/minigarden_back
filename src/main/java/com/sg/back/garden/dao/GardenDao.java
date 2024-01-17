11502a7eb8174362531b5ea9ae7a58fd00e45910
Suyeon12345 <antndus2360@gmail.com>
2024-01-17 19:07:57 +0900

    garden pull

---
+++ src/main/java/com/sg/back/garden/dao/GardenDao.java
@@ -0,0 +1,31 @@
+package com.sg.back.garden.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class GardenDao {
+    Logger logger = LoggerFactory.getLogger(GardenDao.class);
+    @Autowired
+    SqlSessionTemplate sqlSessionTemplate = null;
+
+    public List<Map<String, Object>> getOK(Map<String, Object> rmap) {
+        logger.info("Garden Dao: getOK");
+        List<Map<String, Object>> gList = null;
+        gList = sqlSessionTemplate.selectList("getOK",rmap);
+        return gList;
+    }
+
+    public List<Map<String, Object>> getDeptData() {
+        logger.info("Garden Logic: getDeptData");
+        List<Map<String, Object>> dList = null;
+        dList = sqlSessionTemplate.selectList("getDeptData");
+        return dList;
+    }
+}
