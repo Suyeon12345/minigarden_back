13a782f998b396e1c84a1786d3e61b388e1f920c
jeeyeon <jeeyeon118@gmail.com>
2024-01-18 16:04:16 +0900

    20230118

--- src/main/java/com/sg/back/angel/controller/AngelController.java
+++ src/main/java/com/sg/back/angel/controller/AngelController.java
@@ -1,34 +1,19 @@
-package com.sg.back.garden.controller;
+package com.sg.back.angel.controller;
 
-import com.google.gson.Gson;
-import com.sg.back.garden.logic.GardenLogic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
-
 @RestController
-@RequestMapping("/garden/*")
-public class Garden {
-    Logger logger = LoggerFactory.getLogger(Garden.class);
-
-    @Autowired
-    GardenLogic gardenLogic = null;
+@RequestMapping("/angel/*")
+public class AngelController {
+    Logger logger = LoggerFactory.getLogger(AngelController.class);
     @GetMapping("test")
-    public String test(@RequestParam Map<String, Object> rmap){
-        logger.info("test호출");
-        logger.info(rmap.toString());
-        List<Map<String, Object>> gList = null;
-        gList = gardenLogic.getOK(rmap);
-        Gson g = new Gson();
-        String temp = g.toJson(gList);
-        return temp;
+
+    public String test(){
+        logger.info("AngelController");
+        return null;
     }
 }
