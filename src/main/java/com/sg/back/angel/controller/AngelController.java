95714963dc400d0bf304b58a661aff2c49cb68dc
jeeyeon <jeeyeon118@gmail.com>
2024-01-22 17:26:47 +0900

    All_이지연_240122

--- src/main/java/com/sg/back/angel/controller/AngelController.java
+++ src/main/java/com/sg/back/angel/controller/AngelController.java
@@ -1,19 +1,56 @@
 package com.sg.back.angel.controller;
 
+import com.google.gson.Gson;
+import com.sg.back.angel.logic.AngelLogic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/angel/*")
 public class AngelController {
     Logger logger = LoggerFactory.getLogger(AngelController.class);
-    @GetMapping("test")
+    @Autowired
+    AngelLogic angelLogic;
 
-    public String test(){
-        logger.info("AngelController");
-        return null;
+//    전체 목록조회하기
+    @GetMapping("pgList")
+    public String pgList(@RequestParam Map<String, Object> pmap){
+        logger.info("AngelController-pgList");
+        List<Map<String, Object>> pgList = null;
+        pgList = angelLogic.pgList(pmap);
+        if(pgList.size() == 0){
+            logger.info("조회된 데이터가 없습니다.");
+            return "noData";
+        }
+        logger.info(pgList.toString());
+        Gson g = new Gson();
+        String temp = g.toJson(pgList);
+        return temp;
+//        return "success";
+    }
+//  프로그램 상세 조회하기
+    @GetMapping("pgDetail")
+    public String pgDetail(@RequestParam Map<String, Object> pmap){
+        logger.info("AngelController-pgDetail");
+        List<Map<String, Object>> pgList = null;
+        pgList = angelLogic.pgList(pmap);
+        Gson g = new Gson();
+        String temp = g.toJson(pgList);
+        System.out.println(temp);
+        return temp;
+    }
+
+//    프로그램 등록하기
+    @PostMapping("pgInsert")
+    public String pgInsert(@RequestBody Map<String, Object> pmap){
+        logger.info("AngelController-pgInsert");
+        int result = 0;
+        result = angelLogic.pgInsert(pmap);
+        return String.valueOf(result);
     }
 }
